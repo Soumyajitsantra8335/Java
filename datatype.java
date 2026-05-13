8608b39360c6b8a1f06272337738cf3c217bfa54
Javascript1414 <soumosantra588@gmail.com>
2026-05-13 12:25:02 +0530

    init: s

---
+++ datatype.java
@@ -0,0 +1,17 @@
+public class datatype{
+    public static void main(String[]args){
+        //Numbers
+        byte a=10;
+        byte b=5;
+        byte c=2;
+        System.out.println(a+b+c);
+
+        char d='a';
+        char e='k';
+        char m='l';
+        System.out.println(d+1);
+        System.out.println(e);
+
+    }
+    
+}
