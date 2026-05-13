550ee25990144d51dc72f40af9814e6606f831e3
Javascript1414 <soumosantra588@gmail.com>
2026-05-13 14:54:11 +0530

    s

---
+++ art.java
@@ -0,0 +1,9 @@
+public class art {
+    public static void main(String[] args) {
+        int a=87;
+        int b=25;
+        
+        System.out.println(a+b);
+        
+    }
+}
---
+++ ifelser.java
@@ -0,0 +1,13 @@
+public class ifelser {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+
+        if(a>=b){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+    
+}
