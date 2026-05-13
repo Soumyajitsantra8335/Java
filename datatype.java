e80b3c48828033c1ffd9dff6d958f69c5bc91041
Javascript1414 <soumosantra588@gmail.com>
2026-05-13 14:36:17 +0530

    s

--- datatype.java
+++ datatype.java
@@ -1,16 +1,47 @@
-public class datatype{
-    public static void main(String[]args){
+public class datatype {
+    public static void main(String[] args) {
         //Numbers
-        byte a=10;
-        byte b=5;
-        byte c=2;
-        System.out.println(a+b+c);
+        byte a = 10;
+        byte b = 5;
+        byte c = 2;
+        System.out.println(a + b + c); 
 
-        char d='a';
-        char e='k';
-        char m='l';
-        System.out.println(d+1);
+        //char
+        char d = 'a';
+        char e = 'k';
+        char m = 'l';
+        System.out.println(d + 1);
         System.out.println(e);
+        System.out.println(m + 1);
+        System.out.println("" + e);
+        System.out.println(d + e + m);
+
+    //Float
+        float marks = 65.33f;
+        float high = 325.41f;
+        System.out.println(marks);
+        System.out.println(high);
+
+        //Boolean
+        boolean n=true;
+        boolean o=false;
+        System.out.println(n);
+        System.out.println(o);
+
+        //Short number
+        short p=5412;
+        short q=8514;
+        System.out.println(p);
+        System.out.println(q);
+
+        //Long Number
+        long r=54123654789l;
+        System.out.println(r);
+
+        //Double
+        double val=85.22525;
+        System.out.println(val);
+
 
     }
     
