c2bc73fd8e5288e4675ad2d50c24a45c77b6b26a
Javascript1414 <soumosantra588@gmail.com>
2026-05-14 16:12:04 +0530

    s

---
+++ data3.java
@@ -0,0 +1,10 @@
+public class data3 {
+    public static void main(String[] args) {
+        String[] a={"soumo","santra","sribas"};
+        System.out.println(a);
+        System.out.println(a[2]);
+        
+        }
+    }
+    
+
