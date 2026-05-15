e96a4bb7d57ec899812a9ca021ef25f9e0a61106
Javascript1414 <soumosantra588@gmail.com>
2026-05-15 12:36:43 +0530

    s

---
+++ enumm.java
@@ -0,0 +1,11 @@
+public class enumm {
+    public static void main(String[] args) {
+        enum s{
+            Monday("Nsti Howrah");
+            Tuesday("Radha Krishna");
+            
+        }
+        System.out.println(s.Monday);
+    }
+    
+}
