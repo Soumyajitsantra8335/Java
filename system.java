58d9a54d81df2510434369881be14c678b55c362
Javascript1414 <soumosantra588@gmail.com>
2026-05-15 17:11:39 +0530

    ss

--- system.java
+++ system.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class system {
     public static void main(String[] args) {
         Scanner hello = new Scanner(System.in);
-        System.out.println("provide your input = ");
+        System.out.print("provide your input = ");
 
         var a = hello.nextLine();
         System.out.println(a);
 
-        //var b=hello.nextLine();
-        //System.out.println(b);
+    var b=hello.hasNextLine();
+        System.out.println(b);
     
     }
     
