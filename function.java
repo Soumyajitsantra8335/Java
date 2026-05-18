2b4ab23ea9ce7ef95a9c2bc5ed4d79e57d3b5108
Javascript1414 <soumosantra588@gmail.com>
2026-05-18 12:14:24 +0530

    s

---
+++ function.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class function{
+   static void add(){
+    Scanner src= new Scanner(System.in);
+
+    System.out.println("Addition Function");
+
+        System.out.print("First Number = ");
+        int a = src.nextInt();
+
+        System.out.print("Second Number = ");
+        int b= src.nextInt();
+
+        int sum = a + b;
+        System.out.println("sum result = "+sum);
+
+    }
+    static void Subtract(){
+    Scanner src1= new Scanner(System.in);
+
+    System.out.println("Subtraction function");
+
+        System.out.print("First Number = ");
+        int a = src1.nextInt();
+
+        System.out.print("Second Number = ");
+        int b= src1.nextInt();
+
+        int sub = a - b;
+        System.out.println("Subtract result = "+sub);
+
+    }
+     static void Multiple(){
+    Scanner src1= new Scanner(System.in);
+
+    System.out.println("Multiple function");
+
+        System.out.print("First Number = ");
+        int a = src1.nextInt();
+
+        System.out.print("Second Number = ");
+        int b= src1.nextInt();
+
+        int mul = a * b;
+        System.out.println("Multiple result = "+mul);
+
+    }
+    static void Division(){
+    Scanner src1= new Scanner(System.in);
+
+    System.out.println("Division function");
+
+        System.out.print("First Number = ");
+        int a = src1.nextInt();
+
+        System.out.print("Second Number = ");
+        int b= src1.nextInt();
+
+        int div = a / b;
+        System.out.println("Division result = "+div);
+
+    }
+    static void Modulus(){
+    Scanner src1= new Scanner(System.in);
+
+    System.out.println("Modulus function");
+
+        System.out.print("First Number = ");
+        int a = src1.nextInt();
+
+        System.out.print("Second Number = ");
+        int b= src1.nextInt();
+
+        int mod = a % b;
+        System.out.println("Modulus result = "+mod);
+
+    }
+    
+
+
+
+   public static void main(String[] args){
+      add();
+      Subtract();
+      Multiple();
+      Division();
+      Modulus();
+   }
+
+
+
+   }
+
+
+        
+    
---
+++ sys.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sys {
+    public static void main(String[] args) {
+        Scanner sys=new Scanner(System.in);
+        System.out.print("Enter Yonr name =");
+         var b=sys.nextLine();
+         System.out.println("This is outpt = "+b);
+
+         var c=sys.hasNextInt();
+         System.out.println(c);
+    }
+
+    
+} 
