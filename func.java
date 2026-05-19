c2b075fb29a93a9c8b103228b3ae99b50178f9d5
Javascript1414 <soumosantra588@gmail.com>
2026-05-19 12:15:37 +0530

    s

---
+++ func.java
@@ -0,0 +1,20 @@
+public class func {
+    static int add(int a, int b){
+        return a + b;
+    }
+    static int sub(int a, int b){
+        return a - b;
+    }
+    static int mul(int a, int b){
+        return a * b;
+
+    }
+    static int div(int a, int b){
+        return a / b;
+    } 
+    public static void main(String[] args) {
+        
+        System.out.println(func.add(2,3));
+    }
+    
+}
---
+++ func2.java
@@ -0,0 +1,25 @@
+ public class func2 {
+    int add(int a, int b){
+        return a + b;
+    }
+    int sub(int a, int b){
+        return a - b;
+    }
+    int mul(int a, int b){
+        return a * b;
+
+    }
+    int div(int a, int b){
+        return a / b;
+    } 
+    public static void main(String[] args) {
+        func a = new func();
+        System.out.println(a.add(2,3));
+        System.out.println(a.sub(1,3));
+        System.out.println(a.mul(3,3));
+        System.out.println(a.div(2,3));
+
+    }
+    
+}
+ 
