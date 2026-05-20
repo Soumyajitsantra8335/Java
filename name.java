0eb6ecad9628bd0178c0f8a1d5c23ef903f9c29b
Javascript1414 <soumosantra588@gmail.com>
2026-05-20 17:03:55 +0530

    s

---
+++ name.java
@@ -0,0 +1,20 @@
+public class name {
+    public static void main(String[]args){
+        String[] name = {
+             " *****    *****   *     *  *     *   ***** ",
+            "*        *     *  *     *  **   **  *     *",
+            "*        *     *  *     *  * * * *  *     *",
+            " *****   *     *  *     *  *  *  *  *     *",
+            "      *  *     *  *     *  *     *  *     *",
+            "*     *  *     *  *     *  *     *  *     *",
+            " *****    *****    *****   *     *   ***** "
+
+        };
+        for (int i=0; i<name.length; i++){
+            System.out.println(name[i]);
+        }
+    }
+
+    
+    
+}
