1750e7c503cd6442ffab121b3df3e83393c56102
Javascript1414 <soumosantra588@gmail.com>
2026-05-22 14:45:03 +0530

    s

---
+++ string.java
@@ -0,0 +1,19 @@
+public class string {
+    public static void main(String[] args) {
+       String name= "Soumyajit";
+       System.out.println(name.charAt(0)); 
+       System.out.println(name.charAt(1)); 
+       System.out.println(name.charAt(2)); 
+       System.out.println(name.charAt(3)); 
+       System.out.println(name.charAt(4)); 
+       System.out.println(name.charAt(5)); 
+       System.out.println(name.charAt(6)); 
+       System.out.println(name.charAt(7)); 
+       System.out.println(name.charAt(8)); 
+
+
+        
+
+    }
+    
+}
