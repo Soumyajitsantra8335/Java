d104c2a59de3a6edbf410ca2e37991dd743fd424
Javascript1414 <soumosantra588@gmail.com>
2026-05-22 11:19:24 +0530

    s

--- array.java
+++ array.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class array {
     public static void main(String[] args) {
-        int c[] = {1,2,3,4};
+        int c[] = {1,2,3,4,5,6};
         System.out.println(Arrays.toString(c));
         System.out.println(c[0]);
         System.out.println(c[1]);
         System.out.println(c[2]);
         System.out.println(c[3]);
+        c[3]=5;
+        System.out.println(Arrays.toString(c));
     }
     
 }
