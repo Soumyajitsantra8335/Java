9c1a919993a01de783db67b87f7fc04acbb961ae
Javascript1414 <soumosantra588@gmail.com>
2026-05-22 11:08:53 +0530

    s

--- array.java
+++ array.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class pp {
+public class array {
     public static void main(String[] args) {
         int c[] = {1,2,3,4};
         System.out.println(Arrays.toString(c));
