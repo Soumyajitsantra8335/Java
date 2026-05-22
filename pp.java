037c943eac7c3c939b164de98fc05de3bbd64a93
Javascript1414 <soumosantra588@gmail.com>
2026-05-22 11:07:27 +0530

    d

---
+++ pp.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class pp {
+    public static void main(String[] args) {
+        int c[] = {1,2,3,4};
+        System.out.println(Arrays.toString(c));
+        System.out.println(c[0]);
+        System.out.println(c[1]);
+        System.out.println(c[2]);
+        System.out.println(c[3]);
+    }
+    
+}
+
+
+
+
