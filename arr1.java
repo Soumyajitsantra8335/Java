5b4b47127455e2fd1370434a90b6590eb2ac8a3d
Javascript1414 <soumosantra588@gmail.com>
2026-05-22 11:51:12 +0530

    s

--- arr1.java
+++ arr1.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+//import java.util.ArrayList;
 public class arr1 {
+    public static void main(String[] args) {
+        
     
+    int [][] i={
+        {65,45,6,4,5,6,4,6},
+        {243,6,6,6,6,6,65,9,8,55,63}
+    };
+    i[1][6]=2;
+    i[0][3]=45;
+    System.out.println(Arrays.deepToString(i));
+    
+}
+
+
+
+
+
+
 }
