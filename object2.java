ed1ab240dfc86c269121617d34d32d33ef8fd436
Javascript1414 <soumosantra588@gmail.com>
2026-05-25 11:37:17 +0530

    s

---
+++ object2.java
@@ -0,0 +1,17 @@
+class stu {
+    String name = "Soumyajit"; 
+
+    void soumo(){
+        System.out.println(name);
+    }
+}
+
+    public class object2{
+        public static void main(String[] args) {
+            
+        
+        stu a=new stu();
+        a.soumo();
+    }
+}
+
