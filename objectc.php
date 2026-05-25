59707bea329880d42482eadeb599607afa472e04
Javascript1414 <soumosantra588@gmail.com>
2026-05-25 10:54:47 +0530

    p

---
+++ objectc.php
@@ -0,0 +1,15 @@
+public class objectc{
+    public static void main(String[] args){
+        string name;
+        int age;
+
+        objectc(String a, int b){
+            name = n;
+            age = m;
+
+        }
+        
+
+    }
+    
+}
